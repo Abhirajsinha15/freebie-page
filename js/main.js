e74cedbc076ffc1f47c01364e9d804aeae5da7c6
Abhiraj <abhirajsinha15@gmail.com>
2022-01-29 14:34:54 +0530

    first committ

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function open() {
+    var x = document.getElementById("menu");
+
+    if(x.style.display== "block"){
+
+        (x.style.display== "none")
+    }
+
+    else{
+        (x.style.display== "block")
+    }
+
+
+}
